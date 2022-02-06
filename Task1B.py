9b8af524d34681c12be559073d6baada605e9abb
Jevon Sunandar <jevonsunandar243@gmail.com>
2022-02-06 00:11:32 +0000

    Task1B

--- Task1B.py
+++ Task1B.py
@@ -1,8 +1,19 @@
+from floodsystem.geo import station_by_distance
+from floodsystem.stationdata import build_station_list
+from floodsystem.geo import haversine
+camb = (52.2053, 0.1218)
+lyon = (43.567, 55.333) 
+print(haversine(camb, lyon))
 
-from haversine import haversine, Unit
-import haversine
-def station_by_distance (stations, p): 
+def camb_run (): 
+    stations = build_station_list()
 
-lyon = (45.75, 46,83)
-paris = (34,62, 56,78)
-print (haversine(lyon, paris))
+    camb = (52.2053, 0.1218) 
+    distance = station_by_distance(stations, camb)
+    
+    return distance
+
+closest = (camb_run())[:10]
+print (closest)
+furthest = (camb_run())[-10:]
+print (furthest)
--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,17 +7,32 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
-
-
-
-from haversine import haversine, Unit
 from floodsystem.stationdata import build_station_list
-from . station import MonitoringStation
+
+#create a haversine function that calculate distance between two coordinates
+from math import radians, degrees, sin, cos, asin, acos, sqrt
+def haversine (coordinate_1, coordinate_2): 
+    distance = 6371 * (acos(sin(coordinate_1[0])) * sin(coordinate_2[0]) 
+        + cos(coordinate_1[0]) * cos(coordinate_2[0]) * cos(coordinate_1[1] - coordinate_2[1]))
+    return distance
+
+# p is a tuple 
 
 
 def station_by_distance (stations, p):
     station_distance = []
-    temp_distance = haversine(station.coordinate,p)
-    
+    assert len(p)== 2 
+    for station in stations:
+        temp_tuple = ()
+        temp_distance = haversine(station.coord, p)
+        temp_tuple = (station.name, temp_distance)
+        station_distance.append(temp_tuple)
+    station_distance = sorted_by_key(station_distance, 1)
+    return station_distance
+
+
+
+
+
     
 
