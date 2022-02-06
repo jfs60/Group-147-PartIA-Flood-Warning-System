0ac52fb7744b5d6ef079f7e754282ca898a9b7c6
Jevon Sunandar <jevonsunandar243@gmail.com>
2022-02-06 10:48:22 +0000

    HI

--- Task1B.py
+++ Task1B.py
@@ -1,9 +1,9 @@
 from floodsystem.geo import station_by_distance
 from floodsystem.stationdata import build_station_list
 from floodsystem.geo import haversine
-camb = (52.2053, 0.1218)
-lyon = (43.567, 55.333) 
-print(haversine(camb, lyon))
+#camb = (52.2053, 0.1218)
+#lyon = (43.567, 55.333) 
+#print(haversine(camb, lyon))
 
 def camb_run (): 
     stations = build_station_list()
--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -17,11 +17,14 @@ def haversine (coordinate_1, coordinate_2):
         + cos(coordinate_1[0]) * cos(coordinate_2[0]) * cos(coordinate_1[1] - coordinate_2[1]))
     return distance
 
-# p is a tuple 
+
 
 #Task1B geocode
+#takes in a list of stations and a tuple p and calculate the distance of the station from the tuple p
+#returns a list of name of station and 
 def station_by_distance (stations, p):
     station_distance = []
+    #check if p is a tuple
     assert len(p)== 2 
     for station in stations:
         temp_tuple = ()
@@ -52,17 +55,25 @@ def stations_within_radius (stations, centre, r):
 
 
 #Task1D geo
+station_by_river_dict = {}
+
 def rivers_with_stations (stations): 
     river_set = set()
     for station in stations:
         river_temp = station.river 
         river_set.add(river_temp)
-    
+        
+    station_by_river_dict = river_set
     return river_set
 
+
+
+ 
 def station_by_river (stations): 
     station_by_river_dict = {}
+    
     for station in stations: 
+        
         if station.river in station_by_river_dict : 
             station_by_river_dict[station.river].append(station.name)
             station_by_river_dict[station.river].sort()
