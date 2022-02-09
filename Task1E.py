1b69c4010f592b4372b2dc23be2e9dd204d0a345
Jevon Sunandar <jevonsunandar243@gmail.com>
2022-02-09 21:03:27 +0000

    done milestone 1

--- Task1E.py
+++ Task1E.py
@@ -1,5 +1,5 @@
 from floodsystem.stationdata import build_station_list
 from floodsystem.geo import rivers_by_station_number
 stations = build_station_list()
-print("\n"+str(rivers_by_station_number(stations,7))+"\n")
+print("\n"+str(rivers_by_station_number(stations,10))+"\n")
 
